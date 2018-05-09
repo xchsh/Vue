fb6beaa7f74328ab1ef78ebe3b9faef7ed715658
xchsh <2630517357@qq.com>
2018-05-09 11:49:00 +0800

    vue.js

---
+++ Vue_News_Apps/app.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+		  el: '#test',
+		  data: {
+		    name: 'xchsh!'
+		  }
+		});
+		const vm = new Vue({
+	    el: '#app',
+	    data: {
+	        results: [
+	            {title: '第一条新闻', abstract: '我是第一条新闻'},
+	            {title: '接着是第二条', abstract: '我是第二条新闻'},
+	            {title: '然后是第三条', abstract: '我是第三条新闻'},
+	            {title: '我是最后一条了', abstract: '我是第四条新闻'}
+	        ]
+	    }
+});
